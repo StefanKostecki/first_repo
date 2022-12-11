577868fad2b3719dfa680c7d857a72c40d0ea437
student-wroclaw <student-wroclaw@EDU-WSB.local>
2022-12-11 13:32:04 +0100

    dane_11.12.2022.1330

--- the_app.py
+++ the_app.py
@@ -1,2 +1,8 @@
 def fizzbuzz(number):
-    return 1
+    if number % 3 == 0 and number % 5 == 0:
+        return 'FizzBuzz'
+    elif number % 3 == 0:
+        return 'Fizz'
+    elif number % 5 == 0:
+        return 'Buzz'
+    return number
--- the_test.py
+++ the_test.py
@@ -1,3 +1,8 @@
 from the_app import *
 def test_0():
     assert fizzbuzz(1) == 1
+    assert fizzbuzz(2) == 2
+    assert fizzbuzz(3) == 'Fizz'
+    assert fizzbuzz(4) == 4
+    assert fizzbuzz(5) == 'Buzz'
+    assert fizzbuzz(15) == 'FizzBuzz'
